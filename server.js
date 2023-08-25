d7a1493ffef0c9311c1a89ca7e945e372505c735
Sanchit <sanchit.n71@rediffmail.com>
2023-08-26 04:42:13 +0530

    backend done

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const dotenv = require('dotenv')
 const morgan = require('morgan');
+const connectDB = require('./config/db')
 
 dotenv.config({path: './config/config.env'});
+
+connectDB();
 
 const transactions = require('./routes/transaction')
 
 const app = express();
+
+app.use(express.json());
 
 app.use('/api/v1/transactions',transactions);
 
