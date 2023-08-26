eeaf3f05e8ec54488c48abf9274d623695365afa
Sanchit <sanchit.n71@rediffmail.com>
2023-08-26 16:58:17 +0530

    backend frontend connected

--- client/src/context/GlobalState.jsx
+++ client/src/context/GlobalState.jsx
@@ -1,42 +1,65 @@
-import React, {createContext,useReducer} from "react";  
-import AppReducer from './AppReducer';
+import React, { createContext, useReducer } from "react";
+import AppReducer from "./AppReducer";
+import axios from 'axios';
 
-// Initial State
+// Initial State 
 const initialState = {
-    transactions: [
-        ]
-}
+  transactions: [],
+  error: null,
+  loading: true
+};
 
 // Create Context
 export const GlobalContext = createContext(initialState);
 
 // Provider Component - for accessing the above store
-export const GlobalProvider = ({children}) => {
-    const [state, dispatch] = useReducer(AppReducer, initialState);
+export const GlobalProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AppReducer, initialState);
 
-    // Actions
-    function deleteTransaction(id){
+  // Actions
+
+  async function getTransactions(){
+    try{
+        const res = await axios.get('/api/v1/transactions');
         dispatch({
-            type: 'DELETE_TRANSACTION',
-            payload: id
+            type: 'GET_TRANSACTIONS',
+            payload: res.data.data
         });
     }
-
-    function addTransaction(transaction){
+    catch(err){
         dispatch({
-            type: 'ADD_TRANSACTION',
-            payload: transaction
+            type: 'TRANSACTION_ERROR',
+            payload: err.response.data.error
         });
     }
+  }
 
-    return (
-        <GlobalContext.Provider value={{
-            transactions: state.transactions
-            ,deleteTransaction
-            ,addTransaction
+  function deleteTransaction(id) {
+    dispatch({
+      type: "DELETE_TRANSACTION",
+      payload: id,
+    });
+  }
 
-        }}>
-            {children}
-        </GlobalContext.Provider>
-    )
-}
+  function addTransaction(transaction) {
+    dispatch({
+      type: "ADD_TRANSACTION",
+      payload: transaction,
+    });
+  }
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        error: state.error,
+        loading: state.loading,
+        getTransactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
